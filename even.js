efa4aee597e3671b50329d4438a105ec65d94cfb
Xuan-Vi Trinh-Nguyen <xuanvi.trinhnguyen@gmail.com>
2018-04-20 17:34:27 -0400

    Adds functions exercises

---
+++ even.js
@@ -0,0 +1,12 @@
+function isEven(num) {
+    return num % 2 == 0;
+}
+
+console.log(10 % 2 === 0);
+11 % 2 === 0;
+
+console.log(isEven(10));
+
+var elevenIsEven = isEven(11);
+
+console.log(elevenIsEven);
